00873bb1536cb2e7c747a8ff4d3aa576015d8d2b
An Phuc Thien Nguyen <npta97@gmail.com>
2020-09-14 21:42:14 +0700

    Add AccessToken domain

---
+++ domain/access_token/access_token.go
@@ -0,0 +1,24 @@
+package access_token
+
+import "time"
+
+const (
+	expirationTime = 24
+)
+
+type AccessToken struct {
+	AccessToken string `json:"access_token"`
+	UserId      int64  `json:"user_id"`
+	ClientId    int64  `json:"client_id"`
+	Expire      int64  `json:"expire"`
+}
+
+func GetNewAccessToken() *AccessToken {
+	return &AccessToken{
+		Expire: time.Now().UTC().Add(expirationTime * time.Hour).Unix(),
+	}
+}
+
+func (at *AccessToken) IsExpired() bool {
+	return time.Unix(at.Expire, 0).Before(time.Now().UTC())
+}
